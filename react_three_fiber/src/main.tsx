2089c4c4c69ba96994e8ae81dc06ba70000a4c9c
mochenya <2827075398@qq.com>
2024-10-25 18:07:14 +0800

    init react three fiber

--- react_three_fiber/src/main.tsx
+++ react_three_fiber/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {Canvas} from "@react-three/fiber";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <>
+    <Canvas>
+      <App/>
+    </Canvas>
+  </>
 )
