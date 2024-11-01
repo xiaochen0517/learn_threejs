de208b72b545bbcb6780c36ff21f7cbe508d12db
mochenya <2827075398@qq.com>
2024-11-01 11:21:55 +0800

    update basic debug view

--- react_three_fiber/src/view/DebugView.jsx
+++ react_three_fiber/src/view/DebugView.jsx
@@ -1,6 +1,6 @@
 import {OrbitControls, Plane} from "@react-three/drei";
 import {useThree} from "@react-three/fiber";
-import {useControls} from "leva";
+import {folder, useControls} from "leva";
 import {Perf} from "r3f-perf";
 
 export default function DebugView() {
@@ -10,42 +10,92 @@ export default function DebugView() {
   three.camera.position.set(0, 2, 7);
   three.camera.lookAt(0, 0, 0);
 
-  const {position, cubeColor, cubeVisible} = useControls({
-    // position: {x: 1.5, y: 0, z: 0},
-    position: {
-      value: {x: 1.5, y: 0, z: 0},
-      label: "Position",
-      x: {min: -10, max: 10},
-      y: {min: -10, max: 10},
-      z: {min: -10, max: 10},
-    },
-    cubeColor: {
-      value: "#ff0000ff",
-      label: "Cube Color",
-    },
-    cubeVisible: {
+  const defaultSceneItemsDebugData = {
+    cubeControl: folder({
+        cubePosition: {
+          value: {x: 1.5, y: 0, z: 0},
+          label: "Position",
+          x: {min: -10, max: 10},
+          y: {min: -10, max: 10},
+          z: {min: -10, max: 10},
+        },
+        cubeColor: {
+          value: "#ff0000ff",
+          label: "Cube Color",
+        },
+        cubeVisible: {
+          value: true,
+          label: "Cube Visible",
+        },
+      },
+      {
+        collapsed: true,
+      },
+    ),
+    sphereControl: folder({
+        spherePosition: {
+          value: {x: -1.5, y: 0, z: 0},
+          label: "Position",
+          x: {min: -10, max: 10},
+          y: {min: -10, max: 10},
+          z: {min: -10, max: 10},
+        },
+        sphereColor: {
+          value: "#ffff00ff",
+          label: "Sphere Color",
+        },
+        sphereVisible: {
+          value: true,
+          label: "Sphere Visible",
+        },
+      },
+      {
+        collapsed: true,
+      },
+    ),
+    planeVisible: {
       value: true,
-      label: "Cube Visible",
+      label: "Show Plane",
     },
+    disableOrbitControls: {
+      value: false,
+      label: "Disable Orbit Controls",
+    },
+  };
+
+  const debugData = useControls({
+    defaultSceneData: folder(defaultSceneItemsDebugData, {collapsed: false}),
   });
 
   return <>
     <Perf position="top-left" style={{marginTop: "3rem"}}/>
 
-    <OrbitControls makeDefault/>
+    {debugData.disableOrbitControls ? <></> : <OrbitControls makeDefault/>}
 
     <ambientLight intensity={1.0}/>
     <directionalLight position={[10, 10, 10]}/>
 
-    <mesh position={[position.x, position.y, position.z]} visible={cubeVisible}>
+    <mesh
+      position={[debugData.cubePosition.x, debugData.cubePosition.y, debugData.cubePosition.z]}
+      visible={debugData.cubeVisible}
+    >
       <boxGeometry args={[1.5, 1.5, 1.5]}/>
-      <meshStandardMaterial color={cubeColor}/>
+      <meshStandardMaterial color={debugData.cubeColor}/>
     </mesh>
-    <mesh position-x={-1.5}>
+    <mesh
+      position={[debugData.spherePosition.x, debugData.spherePosition.y, debugData.spherePosition.z]}
+      visible={debugData.sphereVisible}
+    >
       <sphereGeometry args={[1]}/>
-      <meshStandardMaterial color="yellow"/>
+      <meshStandardMaterial color={debugData.sphereColor}/>
     </mesh>
 
-    <Plane args={[10, 10]} rotation-x={-Math.PI / 2} position-y={-1} material-color="skyblue"/>
+    <Plane
+      visible={debugData.planeVisible}
+      args={[10, 10]}
+      rotation-x={-Math.PI / 2}
+      position-y={-1}
+      material-color="skyblue"
+    />
   </>;
 }
