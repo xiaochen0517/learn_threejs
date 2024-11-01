46afe0144f9e16689008cb108f59dad3f2765040
mochenya <2827075398@qq.com>
2024-11-01 11:02:55 +0800

    env and stage code done

--- react_three_fiber/src/view/EnvAndStageView.jsx
+++ react_three_fiber/src/view/EnvAndStageView.jsx
@@ -1,4 +1,16 @@
-import {OrbitControls, Sky, SoftShadows, useHelper} from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  BakeShadows,
+  ContactShadows,
+  Environment,
+  Lightformer,
+  OrbitControls,
+  RandomizedLight,
+  Sky,
+  SoftShadows,
+  Stage,
+  useHelper,
+} from "@react-three/drei";
 import {useFrame, useThree} from "@react-three/fiber";
 import {useControls} from "leva";
 import {Perf} from "r3f-perf";
@@ -14,25 +26,92 @@ export default function EnvAndStageView() {
   three.camera.position.set(0, 2, 7);
   three.camera.lookAt(0, 0, 0);
 
-  const {cubePosition} = useControls({
+  const {
+    cubePosition,
+    openLight,
+    openStage,
+    openEnvMap,
+    openHdrEnvMap,
+    openHdrSunset,
+    openEnvMapGround,
+    openLightformer,
+    openBakeShadows,
+    openPlane,
+    shadowType,
+  } = useControls({
     cubePosition: {
-      value: {x: 2, y: 1, z: 0},
+      value: {x: 2, y: 2, z: 0},
       label: "Cube Position",
       x: {min: -10, max: 10, step: 0.1},
       y: {min: -10, max: 10, step: 0.1},
       z: {min: -10, max: 10, step: 0.1},
     },
+    openLight: {
+      value: true,
+      label: "Open Light",
+    },
+    openStage: {
+      value: false,
+      label: "Open Stage",
+    },
+    openEnvMap: {
+      value: false,
+      label: "Open Environment Map",
+      render: (get) => !get("openStage"),
+    },
+    openHdrEnvMap: {
+      value: false,
+      label: "Open HDR Environment Map",
+    },
+    openHdrSunset: {
+      value: false,
+      label: "Open HDR Sunset",
+      render: (get) => get("openHdrEnvMap"),
+    },
+    openEnvMapGround: {
+      value: false,
+      label: "Open Environment Map Ground",
+      render: (get) => get("openEnvMap"),
+    },
+    openLightformer: {
+      value: false,
+      label: "Open Lightformer",
+    },
+    openBakeShadows: {
+      value: false,
+      label: "Open Bake Shadows",
+    },
+    openPlane: {
+      value: true,
+      label: "Open Plane",
+    },
+    shadowType: {
+      value: "none",
+      label: "Shadow Type",
+      options: ["none", "SoftShadows", "AccumulativeShadows", "ContactShadows"],
+    },
   });
 
   const directionalLightRef = useRef();
-  useHelper(directionalLightRef, THREE.DirectionalLightHelper);
   const directionalLightCameraRef = useRef();
-  useHelper(directionalLightCameraRef, THREE.CameraHelper);
+  useHelper(directionalLightRef, openLight ? THREE.DirectionalLightHelper : null);
+  useHelper(directionalLightCameraRef, openLight ? THREE.CameraHelper : null);
 
   const cubeRef = useRef();
   useFrame((_state, delta) => {
     cubeRef.current.rotation.y += delta;
   });
+
+  const cubeAndSphere = <>
+    <mesh ref={cubeRef} castShadow={true} position={[cubePosition.x, cubePosition.y, cubePosition.z]}>
+      <boxGeometry args={[1.5, 1.5, 1.5]}/>
+      <meshStandardMaterial color="blue"/>
+    </mesh>
+    <mesh castShadow={true} position-x={-2} position-y={1}>
+      <sphereGeometry args={[1]}/>
+      <meshStandardMaterial color="yellow"/>
+    </mesh>
+  </>;
 
   return <>
     <Perf position="top-left" style={{marginTop: "3rem"}}/>
@@ -44,32 +123,63 @@ export default function EnvAndStageView() {
 
     <axesHelper args={[5]}/>
 
-    <Sky/>
+    {!openEnvMap ? <Sky/> : <></>}
 
-    {/*<BakeShadows/>*/}
-    <SoftShadows size={10}/>
-    {/*<AccumulativeShadows position-y={-0.89} scale={10}>*/}
-    {/*  <RandomizedLight amount={8} position={[5, 5, 5]}/>*/}
-    {/*</AccumulativeShadows>*/}
-    {/*<ContactShadows position={[0, -0.89, 0]}/>*/}
+    {openBakeShadows ? <BakeShadows/> : <></>}
+    {(() => {
+      switch (shadowType) {
+        case "SoftShadows":
+          return <SoftShadows size={10}/>;
+        case "AccumulativeShadows":
+          return (
+            <AccumulativeShadows position-y={-0.89} scale={10}>
+              <RandomizedLight amount={8} position={[5, 5, 5]}/>
+            </AccumulativeShadows>
+          );
+        case "ContactShadows":
+          return <ContactShadows position={[0, -0.89, 0]}/>;
+        default:
+          return <></>;
+      }
+    })()}
 
-    <ambientLight intensity={1.0}/>
-    <directionalLight ref={directionalLightRef} castShadow={true} position={[5, 5, 5]} shadow-mapSize={[1024, 1024]}>
-      <orthographicCamera ref={directionalLightCameraRef} attach="shadow-camera" args={[-10, 10, 10, -10]}/>
-    </directionalLight>
+    {openEnvMap ?
+      <Environment
+        background
+        files={openHdrEnvMap && !openHdrSunset ? "/environmentMaps/je_gray_02_2k.hdr" : null}
+        preset={openHdrEnvMap && openHdrSunset ? "sunset" : null}
+        ground={openEnvMapGround ? {
+          height: 7,
+          radius: 28,
+          scale: 100,
+        } : null}
+      >
+        <color args={["lightblue"]} attach="background"/>
+        {openLightformer ?
+          <Lightformer color="white" scale={10} intensity={10} position={[0, 0, -5]} form="ring"/> : <></>
+        }
+      </Environment> : <></>
+    }
 
-    <mesh ref={cubeRef} castShadow={true} position={[cubePosition.x, cubePosition.y, cubePosition.z]}>
-      <boxGeometry args={[1.5, 1.5, 1.5]}/>
-      <meshStandardMaterial color="blue"/>
-    </mesh>
-    <mesh castShadow={true} position-x={-2}>
-      <sphereGeometry args={[1]}/>
-      <meshStandardMaterial color="yellow"/>
-    </mesh>
+    {
+      openLight ? (<>
+        <ambientLight intensity={0.5}/>
+        <directionalLight
+          ref={directionalLightRef}
+          castShadow={true}
+          position={[5, 5, 5]}
+          shadow-mapSize={[1024, 1024]}
+        >
+          <orthographicCamera ref={directionalLightCameraRef} attach="shadow-camera" args={[-10, 10, 10, -10]}/>
+        </directionalLight>
+      </>) : (<></>)
+    }
 
-    <mesh receiveShadow={true} position-y={-0.9} rotation-x={-Math.PI / 2}>
+    {openStage ? <Stage>{cubeAndSphere}</Stage> : cubeAndSphere}
+
+    {!openStage && openPlane ? <mesh receiveShadow={true} position-y={0} rotation-x={-Math.PI / 2}>
       <planeGeometry args={[10, 10]}/>
       <meshStandardMaterial color="skyblue"/>
-    </mesh>
+    </mesh> : <></>}
   </>;
 }
