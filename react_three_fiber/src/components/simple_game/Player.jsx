4a8b78f3b956155cf8b77ff8b4c4a02f3e14e462
mochenya <2827075398@qq.com>
2024-11-11 12:22:59 +0800

    update simple game play component

--- react_three_fiber/src/components/simple_game/Player.jsx
+++ react_three_fiber/src/components/simple_game/Player.jsx
@@ -3,6 +3,9 @@ import {RigidBody} from "@react-three/rapier";
 import {useFrame} from "@react-three/fiber";
 import {useEffect, useRef} from "react";
 
+const IMPULSE_SCALE = 0.4;
+const TORQUE_SCALE = 0.1;
+
 export default function Player() {
 
   const playBodyRef = useRef();
@@ -24,18 +27,16 @@ export default function Player() {
     );
   }, []);
 
-  useFrame((state, delta) => {
+  const noneZero = (v) => v.x !== 0 || v.y !== 0 || v.z !== 0;
+  const doMove = (delta) => {
     if (!playBodyRef.current) {
       return;
     }
-    const {forward, backward, left, right, jump} = getKeys();
-    // rotate the player's body
+    const {forward, backward, left, right} = getKeys();
     const impulse = {x: 0, y: 0, z: 0};
     const torque = {x: 0, y: 0, z: 0};
-
-    const impulseStrength = 0.6 * delta;
-    const torqueStrength = 0.2 * delta;
-
+    const impulseStrength = IMPULSE_SCALE * delta;
+    const torqueStrength = TORQUE_SCALE * delta;
     if (forward) {
       impulse.z -= impulseStrength;
       torque.x -= torqueStrength;
@@ -52,12 +53,19 @@ export default function Player() {
       impulse.x += impulseStrength;
       torque.z -= torqueStrength;
     }
+    if (noneZero(impulse) || noneZero(torque)) {
+      // wake up the body
+      playBodyRef.current.wakeUp();
+    }
     playBodyRef.current.applyImpulse(impulse);
     playBodyRef.current.applyTorqueImpulse(torque);
+  };
+  useFrame((state, delta) => {
+    doMove(delta);
   });
   return <RigidBody
     ref={playBodyRef}
-    colliders="ball"
+    colliders="trimesh"
     restitution={0.2}
     friction={1}
     position={[0, 1, 0]}
